14c99f29990405345af9e36f6580ab7d07a44d93
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-02-20 19:51:23 +0530

    update optimized nqueens.

--- nqueens.java
+++ nqueens.java
@@ -1,62 +1,57 @@
 /*
- * in this java program we are going to learn the n queens problem ,in this problem ,in a chess board where
- * n queens are placed and they are placed in a such a way that no queen will attack another queen
- * as we know that queens attack diagnolly,forward,backward,left,right we have to make sure that no queen
- * should be alligned in the path.but this logic we havae written will take more time
+ * in this program we are going to learn reduce the time complexity of the program as it
+ * is we are going to eliminate the issafe function by creating three boolean arrays.
+. . . . . . . Q
+. . . Q . . . .
+Q . . . . . . .
+. . Q . . . . .
+. . . . . Q . .
+. Q . . . . . .
+. . . . . . Q .
+. . . . Q . . .
+in the following there are 8 queens arranged and let us assume we are comming for first to last row.
+so that we have to only check the column,left diagnol,right diagnol so that we create three boolean 
+array and back track them.
  */
-public class nqueens {
-    
-public static void main(String [] args){
-    int n=4;
-    char[][] arr = new char[n][n];
-    for(int i=0; i<arr.length; i++){
-        for(int j=0; j<arr.length; j++){
-            arr[i][j]='.';
+public class nqueensopt{
+    public static void main(String [] args){
+        int n=8;
+        char[][] arr = new char[n][n];
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                arr[i][j]='.';
+            }
+            
         }
-        
+        nq(arr, 0, new boolean[n], new boolean[2*n-1], new boolean[2*n-1]);
     }
-    nq(arr, 0);
-}
-public static boolean isSafe(char[][] arr, int row, int col){
-    for(int i=0; i<row; i++){
-        if(arr[i][col]=='Q'){
-            return false;
-        }
-    }
-
-    for(int i=row-1, j=col+1; i>=0 && j<arr[0].length; i--,j++){
-        if(arr[i][j]=='Q'){
-            return false;
-        }
-
-    }
-    for(int i=row-1, j=col-1; i>=0 && j>=0; i--,j--){
-        if(arr[i][j]=='Q'){
-            return false;
+   
+    public static void print(char[][] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[i][j] +" ");
+            }
+            System.out.println();
         }
     }
-    return true;
-}
-public static void print(char[][] arr){
-    for(int i=0; i<arr.length; i++){
-        for(int j=0; j<arr[0].length; j++){
-            System.out.print(arr[i][j] +" ");
+    public static void nq(char[][] arr, int row, boolean[] cols, boolean[] d1, boolean[] d2){
+        if (row == arr.length){
+            print(arr);
+            System.out.println("----------------");
+            return;
         }
-        System.out.println();
-    }
-}
-public static void nq(char[][] arr, int row){
-    if (row == arr.length){
-        print(arr);
-        System.out.println("----------------");
-        return;
-    }
-    for(int col=0; col<arr.length; col++){
-        if(isSafe(arr, row, col)==true){
-            arr[row][col]='Q';
-            nq(arr, row+1);
-            arr[row][col]='.';
+        for(int col=0; col<arr[0].length; col++){
+            if(cols[col]==false && d1[row + col]==false && d2[row - col + arr.length-1]==false){
+                arr[row][col]='Q';
+                cols[col]=true;
+                d1[row + col]=true;
+                d2[row - col + arr.length-1]=true;
+                nq(arr, row+1, cols, d1, d2);
+                arr[row][col]='.';
+                cols[col]=false;
+                d1[row + col]=false;
+                d2[row - col + arr.length-1]=false;
+            }
         }
     }
-}
 }
