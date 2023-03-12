e2ee6a4e27b77966861e7d0a408788a2081f5fb6
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-03-12 10:13:06 +0530

    Update fibanoccirecursion.java

--- fibanoccirecursion.java
+++ fibanoccirecursion.java
@@ -5,15 +5,19 @@
  * fibanocci by using memorization that is using another storage array.
  */
 public class fibanoccirecursion {
-    public static int fb(int n){
+    public static int fb(int n, int[] d){
         if(n==0||n==1){
             return n;
         }
-        int fn= fb(n-1) + fb(n-2);
+        if(d[n]!=0){
+            return d[n];
+        }
+        int fn= fb(n-1, d) + fb(n-2 , d);
+        d[n]=fn;
         return fn;
     }
     public static void main(String [] args){
         int n=10;
-        System.out.println(fb(n));
+        System.out.println(fb(n, new int[n+1]));
     }
 }
